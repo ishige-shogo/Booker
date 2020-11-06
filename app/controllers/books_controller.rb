ac9d76b7157673b0542e638b1445eb993b5e2c50
ishige-shogo <shigeshigeishige@gmail.com>
2020-11-06 02:57:35 +0000

    [update]show,edit destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,38 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
     @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to new_book_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
